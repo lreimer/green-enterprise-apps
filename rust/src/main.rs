4a2ffa6f4f95d5324f56da3323e9a58eac3f068f
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-10-14 01:08:49 +0200

    Added Rust impl.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,25 @@
+use std::{
+    io::{prelude::*},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = "Green Enterprise Apps with Rust.";
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
