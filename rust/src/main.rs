d38fc0f5949a8a2193cd6983009c939b95cd6778
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-10-14 09:46:33 +0200

    Added simple Rust server.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-fn main() {
+fn main() {   
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
 
     for stream in listener.incoming() {
